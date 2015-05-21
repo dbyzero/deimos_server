e034203ace2e80e61a94b61c9f7f8dfabc759f63
dbyzero <cyril@dbyzero.com>
2015-05-20 17:21:13 -0700

    Add docker config

--- Config.js
+++ Config.js
@@ -45,6 +45,39 @@ config['localhost'].Scene.width = 675;
 config['localhost'].Scene.height = 500;
 config['localhost'].Scene.blocks =  [  {       "position" : {  "x" : 349,      "y" : 349 },    "height" : 10,  "width" : 100,  "type" : {      "value" : 0,    "type" : "blocks" },    "id" : "block-1-by-id-testBlock",      "vertexTL" : {  "x" : 349,      "y" : 349 },    "vertexTR" : {  "x" : 449,      "y" : 349 },    "vertexBL" : {  "x" : 349,      "y" : 359 },    "vertexBR" : {  "x" : 449,      "y" : 359 } },  {       "position" : {         "x" : 100,      "y" : 400 },    "height" : 100,         "width" : 100,  "type" : {      "value" : 0,    "type" : "blocks" },    "id" : "block-2-by-id-testBlock2",      "vertexTL" : {  "x" : 100,      "y" : 400 },    "vertexTR" : {         "x" : 200,      "y" : 400 },    "vertexBL" : {  "x" : 100,      "y" : 500 },    "vertexBR" : {  "x" : 200,      "y" : 500 } },  {       "position" : {  "x" : 540,      "y" : 250 },    "height" : 200,         "width" : 100, "type" : {      "value" : 0,    "type" : "blocks" },    "id" : "block-3-by-id-testBlock3",      "vertexTL" : {  "x" : 540,      "y" : 250 },    "vertexTR" : {  "x" : 640,      "y" : 250 },    "vertexBL" : {  "x" : 540,      "y" : 450 },  "vertexBR" : {   "x" : 640,      "y" : 450 } },  {       "position" : {  "x" : 500,      "y" : 150 },    "height" : 50,  "width" : 200,  "type" : {      "value" : 0,    "type" : "plateform" },         "id" : "block-4-by-id-testBlock4",    "vertexTL" : {   "x" : 500,      "y" : 150 },    "vertexTR" : {  "x" : 700,      "y" : 150 },    "vertexBL" : {  "x" : 500,      "y" : 200 },    "vertexBR" : {  "x" : 700,      "y" : 200 } } ];
 
+/**************
+/* DOCKER **
+**************/
+
+config['docker'] = {};
+
+config['docker'].Engine = {};
+config['docker'].Engine.MAIN_LOOP_DELAY = 33; //40 = 25FPS, 33 = 33FPS, 20 = 50FPS, 
+config['docker'].Engine.SYNCHRO_MAX_DELAY = 9000;
+config['docker'].Engine.SYNCHRO_MIN_DELAY = 3000;
+config['docker'].Server = {};
+config['docker'].Server.server_port = 80;
+config['docker'].Scene = {};
+config['docker'].Scene.maxInstance = 20;
+config['docker'].Scene.maxUser = 20;
+config['docker'].Scene.minDeltaSaveElement = 5000;
+config['docker'].API = {};
+config['docker'].API.url = 'http://api:80';
+config['docker'].Network = {};
+config['docker'].Network.compression = 'verbose'; // text | verbose
+config['docker'].Client = {};
+//Sqaure distance where the client is the authority (square to speed up calcs)
+config['docker'].Client.SQUARE_AUTHORITY_DISTANCE = 100*100;
+
+
+//MOCKUP
+config['docker'].Scene.name = 'deimos test';
+config['docker'].Scene.domId = 'gamezone';
+config['docker'].Scene.regexUrl = 'http://localhost/deimos_client';
+config['docker'].Scene.width = 675;
+config['docker'].Scene.height = 500;
+config['docker'].Scene.blocks =  [  {       "position" : {  "x" : 349,      "y" : 349 },    "height" : 10,  "width" : 100,  "type" : {      "value" : 0,    "type" : "blocks" },    "id" : "block-1-by-id-testBlock",      "vertexTL" : {  "x" : 349,      "y" : 349 },    "vertexTR" : {  "x" : 449,      "y" : 349 },    "vertexBL" : {  "x" : 349,      "y" : 359 },    "vertexBR" : {  "x" : 449,      "y" : 359 } },  {       "position" : {         "x" : 100,      "y" : 400 },    "height" : 100,         "width" : 100,  "type" : {      "value" : 0,    "type" : "blocks" },    "id" : "block-2-by-id-testBlock2",      "vertexTL" : {  "x" : 100,      "y" : 400 },    "vertexTR" : {         "x" : 200,      "y" : 400 },    "vertexBL" : {  "x" : 100,      "y" : 500 },    "vertexBR" : {  "x" : 200,      "y" : 500 } },  {       "position" : {  "x" : 540,      "y" : 250 },    "height" : 200,         "width" : 100, "type" : {      "value" : 0,    "type" : "blocks" },    "id" : "block-3-by-id-testBlock3",      "vertexTL" : {  "x" : 540,      "y" : 250 },    "vertexTR" : {  "x" : 640,      "y" : 250 },    "vertexBL" : {  "x" : 540,      "y" : 450 },  "vertexBR" : {   "x" : 640,      "y" : 450 } },  {       "position" : {  "x" : 500,      "y" : 150 },    "height" : 50,  "width" : 200,  "type" : {      "value" : 0,    "type" : "plateform" },         "id" : "block-4-by-id-testBlock4",    "vertexTL" : {   "x" : 500,      "y" : 150 },    "vertexTR" : {  "x" : 700,      "y" : 150 },    "vertexBL" : {  "x" : 500,      "y" : 200 },    "vertexBR" : {  "x" : 700,      "y" : 200 } } ];
+
 
 //Script d'amalyse des arguments
 var args = {};
